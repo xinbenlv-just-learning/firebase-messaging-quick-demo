8086cb3e508df701a828d46d4964e769929f4fde
Zainan Victor Zhou <xinbenlv@gmail.com>
2016-05-31 22:42:46 -0700

    Done

---
+++ Pods/Target Support Files/Pods-FCMSwift/Pods-FCMSwift-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_FCMSwiftVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_FCMSwiftVersionString[];
+
---
+++ Pods/Target Support Files/Pods-FCMTests/Pods-FCMTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FCMTests : NSObject
+@end
+@implementation PodsDummy_Pods_FCMTests
+@end
